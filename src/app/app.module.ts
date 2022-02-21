7e9c72cb53c776cc1159b199bda7b4d92e945561
Hiagoleresdev <hiagoleresdev@gmail.com>
2022-02-21 15:54:31 -0300

    Terminando forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Form2Component } from './form2/form2.component';
+
+import { TextMaskModule } from 'angular2-text-mask';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    Form2Component
   ],
   imports: [
+    NgxMaskModule.forRoot(),
+    TextMaskModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder, Validators,FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  formulario!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.formulario = new FormGroup({
+     nome: new FormControl(null,Validators.required),
+     email: new FormControl(null,Validators.required),
+     senha: new FormControl(null,Validators.required),
+     cpf : new FormControl(null, Validators.required)
+    });
+  }
+
+  postar() {
+    if (!this.formulario.valid) {
+      console.log("Formulário inválido");
+      return;
+    }
+    console.log("Formulário válido", this.formulario.value);
+  }
+  }
+
+
+
+
+
+
